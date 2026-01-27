9ae241bfac44b99ddb9cd9da6550f04f413c43a8
MarcovdKOelen <fencing.mvk@gmail.com>
2026-01-27 15:44:48 +0100

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,30 +1,28 @@
-// GENERATED FILE (manual) - Firebase options for Trainingsplanung Fechten MvK
-// Android package: de.deinname.fechtplanung
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      // Web not configured in this project
       throw UnsupportedError('DefaultFirebaseOptions are not configured for web.');
     }
+
     switch (defaultTargetPlatform) {
-  case TargetPlatform.android:
-    return android;
-  case TargetPlatform.iOS:
-  case TargetPlatform.macOS:
-  case TargetPlatform.windows:
-  case TargetPlatform.linux:
-  case TargetPlatform.fuchsia:
-    throw UnsupportedError(
-      'DefaultFirebaseOptions are not configured for this platform.',
-    );
-}
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+      case TargetPlatform.fuchsia:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not configured for this platform.',
+        );
+    }
+  }
 
-
-  // Values taken from google-services.json
+  // Android values from google-services.json
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: "AIzaSyB8BUWz6JrgTSDKEEGKfbSyllOA_1uecPo",
     appId: "1:630196522503:android:59e992b16dd15f0b54a4a2",
