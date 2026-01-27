0c04dc21aa8c5074e61ebfbdadea351b40e2202b
MarcovdKOelen <fencing.mvk@gmail.com>
2026-01-27 14:47:14 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trainingsplanung Fechten MvK',
+      theme: ThemeData(useMaterial3: true),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Trainingsplanung Fechten MvK',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
