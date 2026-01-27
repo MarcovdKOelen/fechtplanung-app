7225a94e864d56706b4c3b61310601fb4f57e003
MarcovdKOelen <fencing.mvk@gmail.com>
2026-01-27 16:37:47 +0100

    Update week_plan_screen.dart

--- lib/ui/week_plan_screen.dart
+++ lib/ui/week_plan_screen.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/age_class.dart';
+import '../models/week_plan.dart';
+import '../services/plan_engine.dart';
 return Scaffold( // <-- const HIER entfernen
   appBar: AppBar(
     title: const Text("Wochenplan"),
