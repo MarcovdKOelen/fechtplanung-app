a74cdfedf8543b60acd87e16f9cfb1bc3848e125
MarcovdKOelen <fencing.mvk@gmail.com>
2026-01-27 14:09:17 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trainingsplanung Fechten MvK',
+      theme: ThemeData(useMaterial3: true),
+      home: const Scaffold(
+        body: Center(child: Text('Trainingsplanung Fechten MvK')),
+      ),
+    );
+  }
+}
