d525403c22aa36320ea21a4d3cdc0a61bfa8f567
MarcovdKOelen <fencing.mvk@gmail.com>
2026-01-27 16:00:58 +0100

    Create age_class.dart

---
+++ lib/models/age_class.dart
@@ -0,0 +1,23 @@
+enum AgeClass { u13, u15, u17, u20 }
+
+String ageClassLabel(AgeClass a) {
+  switch (a) {
+    case AgeClass.u13:
+      return "U13";
+    case AgeClass.u15:
+      return "U15";
+    case AgeClass.u17:
+      return "U17";
+    case AgeClass.u20:
+      return "U20";
+  }
+}
+
+AgeClass? parseAgeClass(String s) {
+  final t = s.trim().toUpperCase();
+  if (t.contains("U13")) return AgeClass.u13;
+  if (t.contains("U15")) return AgeClass.u15;
+  if (t.contains("U17")) return AgeClass.u17;
+  if (t.contains("U20")) return AgeClass.u20;
+  return null;
+}
